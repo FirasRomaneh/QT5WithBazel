5e43ebba65d0b8ac06837a0e30bf1866e7a87431
FirasRomaneh <firasrom12345@gmail.com>
2022-08-02 05:25:02 +0300

    QtWithOpencv

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -5,8 +5,6 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   MainWindow w;
-  int i;
   w.show();
-
   return a.exec();
 }
--- hello_world/mainwindow.cpp
+++ hello_world/mainwindow.cpp
@@ -1,7 +1,13 @@
 #include "hello_world/mainwindow.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent)
 {
   ui.setupUi(this);
+  cv::Mat img;
+  img = cv::imread("hello_world/test.jpg");
+  // std::cout << img.size();
+  ui.label->setPixmap(QPixmap::fromImage(QImage(img.data, img.cols, img.rows, img.step, QImage::Format_RGB888)));
 }
